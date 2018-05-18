392e37af8c917c8ff6388f6f7b3c5f5a8148e088
lcofjp <lcofjp@163.com>
2018-05-18 18:49:31 +0800

    update index.js

--- vscode-api/index.js
+++ vscode-api/index.js
@@ -8,3 +8,18 @@ const htmlContent = fs.readFileSync('./vscode-api.html', {encoding: 'utf-8'})
 
 const $ = cheerio.load(htmlContent)
 
+var apiList = []
+
+$('h3').each((i, e) => {
+    const text = $(e).text()
+    console.log(`${i}: ${text}`)
+    apiList.push(text)
+})
+
+var len = apiList.length
+
+console.log(len)
+
+// apiList = apiList.filter(i => /work/i.test(i))
+
+// console.log(apiList)
