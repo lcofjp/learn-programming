cbaf06739b4e0db54ce369dd40308176b88ad10f
lcofjp <lcofjp@163.com>
2018-07-22 11:56:18 +0800

    add book-list

---
+++ fetch-web-content/index.js
@@ -0,0 +1,72 @@
+const cheerio = require('cheerio')
+const request = require('request')
+
+// testUrl: https://download.csdn.net/user/u011433684/uploads/1
+
+function fetchWebContent(url) {
+  return new Promise(function(resolve, reject) {
+    request(url, function (error, response, body) {
+      if (error) {
+        reject(error);
+      }
+      if (response && response.statusCode != 200) {
+        reject(new Error(`Error: status code: ${response.statusCode}`))
+      }
+      resolve(body);
+    });
+  })
+}
+
+module.exports = {
+  fetchWebContent
+}
+
+function fetchTitles(url) {
+  //const testUrl = 'https://download.csdn.net/user/u011433684/uploads/1'
+  return fetchWebContent(url)
+  .then(content => {
+    const $ = cheerio.load(content);
+    var titles = $('li h3').map((index, item) => {
+      return $(item).text()
+    }).get()
+    return titles;
+  })
+}
+
+const getNextPage = (function(n) {
+  var i = 1;
+  return function() {
+    if (i <= n) {
+      return i++;
+    } else {
+      return null;
+    }
+  }
+})(500);
+
+var arr = [];
+
+function fetchPage(pageNum) {
+  var nextUrl;
+  if (pageNum == null) {
+    var allTitles = JSON.stringify(arr, null, '  ');
+    var fs = require('fs');
+    fs.writeFileSync('books-list.txt', allTitles);
+    return;
+  } else {
+    nextUrl = `https://download.csdn.net/user/u011433684/uploads/${pageNum}`
+  }
+  fetchTitles(nextUrl).then(titles => {
+    console.log(pageNum, titles);
+    arr =[...arr, ...titles]
+    fetchPage(getNextPage());
+  }).catch(err => {
+    console.log(err);
+  })
+}
+
+
+fetchPage(getNextPage());
+
+
+// test();
