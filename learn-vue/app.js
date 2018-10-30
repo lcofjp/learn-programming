fbf7e5d19168bf81e6eed8481f576800c036f985
lcofjp <lcofjp@163.com>
2018-10-30 19:26:28 +0800

    change code

--- learn-vue/app.js
+++ learn-vue/app.js
@@ -19,13 +19,33 @@
   * destroyed
   */
 
+/**
+ * 指令:参数.修饰符
+ * v-on:submit.prevent="onSubmit"
+ * 简写：
+ * 1. v-bind:href="url"  ==> :href="url"
+ * 2. v-on:click="handler" ==> @click="handler"
+ */
+
 var vm = new Vue({
     el: '#root1',
     data: {
-        a: 1
+        a: 1,
+        c: 3
+    },
+    // 计算属性，用于从data计算得到的属性
+    // 计算属性是响应求值的，只有它依赖的属性发生变化才会重新求值，否则会使用缓存的值。
+    // now没有依赖，只会求值一次，不会刷新；如果不想要缓存，可以做成函数放在methods里。
+    computed: {
+        cplus1: function(){
+            return this.c + 1;
+        },
+        now: function(){
+            return Date.now();
+        }
     },
     methods: {
-        'click': function(){console.log('click'); this.a++;}
+        click: function(){console.log('click'); this.a++;}
     },
     created: function() {
         // this === vm
